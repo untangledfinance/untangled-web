fb51e3821f8737c033815eab0d8bb40349af7e88
vi <gmail@tuanm.dev>
2025-09-10 17:36:30 +0700

    chore: pass raw body to proxy request

--- src/core/http/server/express.ts
+++ src/core/http/server/express.ts
@@ -41,6 +41,7 @@ class Helper {
         queryStringPosition >= 0 && req.url.slice(queryStringPosition),
       params: req.params,
       body: req.body,
+      rawBody: (req as any).rawBody as string,
     });
   }
 
@@ -111,19 +112,38 @@ export abstract class Group implements Router {
   /**
    * Tries sending the {@link Res}.
    */
-  protected send(res: express.Response, response: Res | Response) {
+  protected async send(res: express.Response, response: Res | Response) {
     if (!res.writableEnded && !(res as any)[RoutedSymbol]) {
       (res as any)[RoutedSymbol] = true;
+
       if (response instanceof Response) {
         response.headers.forEach((value, key) => {
-          if (key.toLowerCase() !== 'content-encoding') {
+          if (
+            !['content-encoding', 'content-length'].includes(key.toLowerCase())
+          ) {
             res.setHeader(key, value);
           }
         });
         res.status(response.status);
-        // @ts-ignore
-        return Readable.fromWeb(response.body).pipe(res);
+
+        if (response.body) {
+          logger.debug(`Streaming response body...`);
+          return response.body.pipeTo(
+            new WritableStream({
+              write(chunk) {
+                res.write(chunk);
+              },
+              close() {
+                logger.debug(`Response body stream ended`);
+                res.end();
+              },
+            })
+          );
+        }
+
+        return res.end();
       }
+
       if (response instanceof ResObj) {
         const { data, status, headers } = response;
         for (const key of Object.keys(headers ?? {})) {
@@ -131,6 +151,7 @@ export abstract class Group implements Router {
         }
         return res.status(status).send(data);
       }
+
       return res.send(response as any);
     }
   }
@@ -148,7 +169,10 @@ export abstract class Group implements Router {
           res: express.Response,
           next: express.NextFunction
         ) => {
-          this.send(res, await fetch(Helper.toReq(req), Helper.toRes(res)));
+          await this.send(
+            res,
+            await fetch(Helper.toReq(req), Helper.toRes(res))
+          );
           next();
         };
         const method = route.method;
@@ -194,12 +218,15 @@ export abstract class Group implements Router {
         next: express.NextFunction
       ) => {
         try {
-          this.send(res, await handle(Helper.toReq(req), Helper.toRes(res)));
+          await this.send(
+            res,
+            await handle(Helper.toReq(req), Helper.toRes(res))
+          );
         } catch (err) {
           if (!(err instanceof HttpError)) {
             logger.error(`${err.message}\n`, err);
           }
-          this.send(res, await this.errorHandler(err, Helper.toReq(req)));
+          await this.send(res, await this.errorHandler(err, Helper.toReq(req)));
         }
         next();
       }
@@ -325,7 +352,22 @@ export abstract class Application extends Group implements Server, OnStop {
     this.emit('start');
     const app = express()
       .options('*', Helper.corsHandler(this.corsOptions))
+      .use(
+        express.urlencoded({
+          extended: true,
+        })
+      )
       .use(express.json())
+      .use((req: express.Request & { rawBody?: string }) => {
+        req.rawBody = '';
+        req.setEncoding('utf8');
+        req.on('data', function (chunk) {
+          req.rawBody += chunk;
+        });
+        req.on('end', function () {
+          req.next();
+        });
+      })
       .use(
         (
           req: express.Request,
@@ -348,7 +390,10 @@ export abstract class Application extends Group implements Server, OnStop {
         ) => {
           const { req } = HttpContext.get();
           this.errorHandler &&
-            this.send(res, await this.errorHandler(new NotFoundError(), req));
+            (await this.send(
+              res,
+              await this.errorHandler(new NotFoundError(), req)
+            ));
           next();
         }
       )
